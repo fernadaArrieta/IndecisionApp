a3761aa29f9189098a56fe0480b91b9b9f957f28
FernadaArrieta <arrietafernanda6@gmail.com>
2023-01-19 11:34:23 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Indecision/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Indecision from '@/components/indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Indecision,
   }
 }
 </script>
